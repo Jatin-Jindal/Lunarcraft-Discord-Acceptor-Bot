5ab66fd553f9fe1a645ec60960493d55409d355a
Jatin Jindal <jatinjindal.2003@gmail.com>
2022-04-30 15:51:21 +0530

    Better Ping

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -4,20 +4,26 @@ const wait = require("node:timers/promises").setTimeout;
 module.exports = {
     data: new SlashCommandBuilder().setName("ping").setDescription("Replies with Latency!"),
     async execute(interaction) {
-        let latency = interaction.createdAt - Date.now();
         const delay = 250;
-        await interaction.reply({
+        var msg = await interaction.reply({
             content: `Calculating Latest Latency`,
             ephemeral: true,
+            fetchReply: true,
         });
+        const latency = msg.createdTimestamp - interaction.createdTimestamp - delay;
         for (let i = 0; i < 3; i++) {
             await wait(delay);
-            msg = await interaction.fetchReply();
-            await interaction.editReply({ content: `${msg.content}.`, ephemeral: true });
+            msg = await interaction.editReply({
+                content: `${msg.content}.`,
+                ephemeral: true,
+                fetchReply: true,
+            });
         }
         await wait(delay);
         await interaction.editReply({
-            content: `${interaction.user.username}'s latecy: ${latency}ms`,
+            content:
+                `Bot's Latency = ${latency}ms\n` +
+                `Websocket Latency = ${interaction.client.ws.ping}ms`,
             ephemeral: true,
         });
     },
