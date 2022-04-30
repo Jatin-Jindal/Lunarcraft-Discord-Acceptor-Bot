ee7bb8d675efaabe114342192f4c8877c0822b68
Jatin Jindal <jatinjindal.2003@gmail.com>
2022-04-30 15:52:34 +0530

    Refactoring and checking nicname length

--- src/commands/accept.js
+++ src/commands/accept.js
@@ -12,9 +12,12 @@ module.exports = {
             option.setName("username").setDescription("The member's in-game username.")
         ),
     async execute(interaction) {
+        // TODO: Check if the user has the required permissions
+        // Permissions: (Have "Whitelister" role AND Send in ADMIN category) or "ADMINISTRATOR"
         const user = interaction.options.getUser("member");
         const member = interaction.guild.members.cache.find((member) => member.id === user.id);
         const username = interaction.options.getString("username");
+        const usernameLength = 32;
         await interaction.deferReply();
         const hasRole = (roleId) => member.roles.cache.find((role) => role.id == roleId);
         rolesRequired = {
@@ -29,6 +32,7 @@ module.exports = {
             Object.values(rolesRequired).forEach((roleId) => member.roles.remove(roleId));
             member.roles.add(roleToAdd);
 
+            // TODO: Send a request to actually whitelist the player IF username is provided
             // TODO: React to the user's message with a checkmark in Application channel
 
             // TODO: Send the following embed in the log channel
@@ -38,16 +42,20 @@ module.exports = {
                     `${member.user} has been accepted into the server by ${interaction.user}.`
                 )
                 .setColor("#7BFA5A");
-            interaction.guild.channels.cache.get("968805196098592801").send({ embeds: [logEmbed] });
-
             if (username && member.displayName !== username) {
-                member.setNickname(username);
+                const nickname =
+                    username.length > usernameLength
+                        ? username.substring(0, usernameLength)
+                        : username;
+                member.setNickname(nickname);
                 logEmbed.addField(
                     "Username Changed",
                     `${member.user}'s username was changed to ${username}.`
                 );
             }
+            interaction.guild.channels.cache.get("968805196098592801").send({ embeds: [logEmbed] });
             await interaction.editReply(`${member} has been accepted into the server.`);
+
             // Inform the user that they have been accepted
             const acceptMessageStuff = {
                 general: "657605715921469480",
@@ -55,7 +63,12 @@ module.exports = {
                 claims: "869554027745148948",
                 mcVersion: "1.17.1",
             };
-            const informMessage = `Hello ${member} and welcome to WolfCraft, we’re glad to have you here. \n\nSome of the key information you’ll need to know is in the “important information tab” you can find reaction roles and link to the live map as well as links to our many different advertising websites. I hope if you enjoy your time here you’ll be kind enough to look through them and upvote 🙂 you can also use prefixes &ip and &map in <#${acceptMessageStuff.general}>  or <#${acceptMessageStuff.botCommands}> , just so you know, the server is ${acceptMessageStuff.mcVersion}.\n\nI would also take a quick look at our <#${acceptMessageStuff.claims}> channel, this is how we “claim” land so there isn’t any confusion on who’s stuff is who’s. This is completely trust based, but we do have ways to make sure your claim stays safe (:\n\nBefore joining the server I would recommend going through the rules one more time so you can be absolutely sure you won’t accidentally break any of them! \n\n- if you have any questions ask in the chat or server help 😄`;
+            const informMessage =
+                `Hello ${member} and welcome to WolfCraft, we’re glad to have you here. \n\n` +
+                `Some of the key information you’ll need to know is in the “Important Information” category you can find reaction roles and link to the live map as well as links to our many different advertising websites. I hope if you enjoy your time here you’ll be kind enough to look through them and upvote 🙂 you can also use prefixes &ip and &map in <#${acceptMessageStuff.general}>  or <#${acceptMessageStuff.botCommands}> , just so you know, the server is ${acceptMessageStuff.mcVersion}.\n\n` +
+                `I would also take a quick look at our <#${acceptMessageStuff.claims}> channel, this is how we “claim” land so there isn’t any confusion on who’s stuff is who’s. This is completely trust based, but we do have ways to make sure your claim stays safe (:\n\n` +
+                `Before joining the server I would recommend going through the rules one more time so you can be absolutely sure you won’t accidentally break any of them! \n\n` +
+                `- if you have any questions ask in the chat or server help 😄`;
             interaction.guild.channels.cache.get("968805196098592801").send(informMessage);
         } else {
             await interaction.editReply(
@@ -65,10 +78,5 @@ module.exports = {
             );
         }
         // console.log(member.roles.cache);
-
-        /*
-            await sendChannel.send(
-                f'{member.mention} you have been accepted!\n\nIP Address: wolfcraft.mcserver.us\nYou should be seeing a lot more channels in the discord! We have an {(discord.utils.get(ctx.guild.text_channels, name = "in-game-bridge")).mention} where you can talk to players online and vice-versa, a {(discord.utils.get(ctx.guild.text_channels, name = "live-map")).mention} of our world and some other great features!')
-                */
     },
 };
